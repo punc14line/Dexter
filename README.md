1279745fc74cb715259b6c92a9df5b98a02a2468
punch14line <matthew.hacker14@gmail.com>
2019-03-07 15:14:13 -0800

    Uploading files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# Dexter" 
+# Dexter
+
+Dexter is a command line executable that will translate the Unix Timestamp in a given log file to a more readable one. Instead of overwriting over the provided log file Dexter will create a new file and write the previous text to the new log file.
+
+## Example
+
+Calling the following command from command line will cause Dexter to read from "SampleLog.txt" and create and write to "Samplelogout.txt"
+> Dexter.exe "-i:SampleLog.txt" "-o:Samplelogout.txt"
---
+++ src/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Dexter
+{
+    class Program
+    {
+
+        static string Timestamp2Readable(string s) => 
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(int.Parse(s)).ToString("r", CultureInfo.CreateSpecificCulture("en-US"));
+
+        static string FindTimestamp(string s)
+        {
+            string pattern = "\\d{10}";
+            Match match = Regex.Match(s, pattern);
+
+            while (match.Success)
+            {
+                s = Regex.Replace(s, pattern, Timestamp2Readable(match.Value));
+                match = Regex.Match(s, pattern);
+            }
+
+            return s;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string line = string.Empty;
+                string input = string.Empty;
+                string output = string.Empty;
+
+                if (args.Length < 2)
+                {
+                    throw new MissingFieldException();
+                }
+
+                input  = (args[0].IndexOf("-i:") != -1) ? args[0].Substring(3) : input;
+                input  = (args[1].IndexOf("-i:") != -1) ? args[1].Substring(3) : input;
+                output = (args[0].IndexOf("-o:") != -1) ? args[0].Substring(3) : output;
+                output = (args[1].IndexOf("-o:") != -1) ? args[1].Substring(3) : output;
+
+                if (input == string.Empty || output == string.Empty)
+                {
+                    throw new MissingFieldException();
+                }
+
+                StreamReader f = new StreamReader(input);
+                StreamWriter file = new StreamWriter(output);
+
+                while ((line = f.ReadLine()) != null)
+                {
+                    file.WriteLine(FindTimestamp(line));
+                }
+
+                file.Close();
+                f.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: was not able to locate \"" + e.FileName + "\"");
+            }
+            catch (MissingFieldException)
+            {
+                Console.WriteLine("Error: Missing parameters");
+                Console.WriteLine("\nRequired Parameters:");
+                Console.WriteLine(" - \"-i:<Input filename>\"");
+                Console.WriteLine(" - \"-o:<Output filename>\"");
+                Console.WriteLine("Example: Dexter.exe \"-i:Log.txt\" \"-o:OutLog.txt\"");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+        }
+    }
+}
